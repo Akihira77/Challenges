0b60c36c161346f537897a07fe6711ef27241f51
Akihira77 <tik.dikawahyu123@gmail.com>
2022-05-14 23:12:49 +0700

    second save: mengganti js dengan css :active

--- 3-column-preview-card-component-main/3-column-preview-card-component-main/myCode/js/script.js
+++ 3-column-preview-card-component-main/3-column-preview-card-component-main/myCode/js/script.js
@@ -1,13 +1,15 @@
-let arrColor = ["--bright-orange", "--dark-cyan", "--very-dark-cyan"];
-const btnLearn = document.querySelectorAll(".btn-learn a");
-const r = document.querySelector(":root");
+// ! javascript dapat tidak digunakan karena dapat menggunaakn css :active
 
-btnLearn.forEach((e, i) => {
-  e.addEventListener("click", () => {
-    let rs = getComputedStyle(r);
-    let btnField = document.querySelectorAll(".btn-learn")[i];
-    btnField.style.backgroundColor = rs.getPropertyValue(arrColor[i]);
-    e.style.color = "lightgray";
-    btnField.style.border = "2px solid lightgray";
-  });
-});
+// let arrColor = ["--bright-orange", "--dark-cyan", "--very-dark-cyan"];
+// const btnLearn = document.querySelectorAll(".btn-learn a");
+// const r = document.querySelector(":root");
+
+// btnLearn.forEach((e, i) => {
+//   e.addEventListener("click", () => {
+//     let rs = getComputedStyle(r);
+//     let btnField = document.querySelectorAll(".btn-learn")[i];
+//     btnField.style.backgroundColor = rs.getPropertyValue(arrColor[i]);
+//     e.style.color = "lightgray";
+//     btnField.style.border = "2px solid lightgray";
+//   });
+// });
